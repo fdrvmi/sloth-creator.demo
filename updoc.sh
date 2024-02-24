ef35a4892a9d0dfefdf80cd157b9a15cd4ac09ea
fdrvmi <fedorov.m2002@yandex.com>
2024-02-24 18:32:58 +0300

    Add doc

---
+++ updoc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo "${GREEN}Генерация документации...${NC}"
+xcodebuild docbuild -scheme SlothCreator -destination 'name=iPhone 14 Pro' -derivedDataPath ./build > /dev/null
+
+echo "${GREEN}Установка...${NC}"
+if [ ! -d "./doc" ]; then
+  mkdir -p "./doc"
+fi
+
+cp -r "./build/Build/Products/Debug/SlothCreator.doccarchive" "./doc" > /dev/null
+
+echo "${RED}Удаление не нужных папок...${NC}"
+rm -r ./.build  > /dev/null
+rm -r ./build  > /dev/null
+
+rm -r ./swiftpm > /dev/null
+
+echo -e "${GREEN}Генерация документации завершена...${NC}"
