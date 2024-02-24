f674ffeed2177ebf2e6917783fbb74d09bee49ed
fdrvmi <fedorov.m2002@yandex.com>
2024-02-24 19:17:42 +0300

    update build script

--- updoc.sh
+++ updoc.sh
@@ -5,14 +5,16 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo "${GREEN}Генерация документации...${NC}"
-xcodebuild docbuild -scheme SlothCreator -destination 'name=iPhone 14 Pro' -derivedDataPath ./build > /dev/null
+xcodebuild docbuild -scheme SlothCreator -destination 'platform=iOS Simulator,name=iPhone 13' -derivedDataPath ./build > /dev/null
 
 echo "${GREEN}Установка...${NC}"
 if [ ! -d "./docs" ]; then
   mkdir -p "./docs"
 fi
 
-cp -r "./build/Build/Products/Debug/SlothCreator.doccarchive/" "./docs" > /dev/null
+# cp -r "./build/Build/Products/Debug/SlothCreator.doccarchive" "./docs/" > /dev/null
+docc=$(xcrun --find docc)
+${docc} process-archive transform-for-static-hosting './build/Build/Products/Debug/SlothCreator.doccarchive' --output-path './docs' --hosting-base-path sloth-creator.demo
 
 echo "${RED}Удаление не нужных папок...${NC}"
 rm -r ./.build  > /dev/null
